dcb733f691bfd71fa87c095740e5e63b10f45966
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-16 12:26:39 -0300

    oiiiii

--- timerBanheiro/Controllers/FuncionariosController.cs
+++ timerBanheiro/Controllers/FuncionariosController.cs
@@ -7,45 +7,61 @@ namespace timerBanheiro.Controllers
     {
         private static List<FuncionariosModel> funcionario = new List<FuncionariosModel>
         {
-            public IActionResult Index()
+
+            new FuncionariosModel
             {
-                new FuncionariosModel
-                {
-                    id = 1,
-                    nome = "Jorge",
-                    sexo = "H",
-                    statusIdade = "2",
-                    pcd = 0,
+                id = 1,
+                nome = "Jorge",
+                sexo = "H",
+                statusIdade = "2",
+                pcd = 0,
+                tempoSecs = 60 + 30
 
-                },
-                new FuncionariosModel
-                {
-                    id = 2,
-                    nome = "Renato Aragão",
-                    sexo = "H",
-                    statusIdade = "3",
-                    pcd = 0,
-                },
-                new FuncionariosModel
-                {
-                    id = 3,
-                    nome = "Amanda Amada",
-                    sexo = "F",
-                    statusIdade = "2",
-                    pcd = 0,
-                };
-                new FuncionariosModel
-                {
-                    id = 4,
-                    nome = "Lorena",
-                    sexo = "F",
-                    statusIdade = "2",
-                    pcd = 1,
-                },
-                return View();
+            },
+
+            new FuncionariosModel
+            {
+                id = 2,
+                nome = "Renato Aragão",
+                sexo = "H",
+                statusIdade = "3",
+                pcd = 0,
+                tempoSecs = 60 + 60
+            },
+
+            new FuncionariosModel
+            {
+                id = 3,
+                nome = "Amanda Amada",
+                sexo = "F",
+                statusIdade = "2",
+                pcd = 0,
+                tempoSecs = 60 + 30 + 30
+            },
+
+            new FuncionariosModel
+            {
+                id = 4,
+                nome = "Lorena",
+                sexo = "F",
+                statusIdade = "2",
+                pcd = 1,
+                tempoSecs = 60 + 60 + 30 + 30
             }
-        }
-    }
+        };
 
+        public IActionResult Index()
+        {
+           return View(funcionario);
+        }
+
+    }
 };
+
+
+
+    
+
+
+
  
--- timerBanheiro/Models/FuncionariosModel.cs
+++ timerBanheiro/Models/FuncionariosModel.cs
@@ -6,9 +6,9 @@
         public string nome { get; set; } = string.Empty;
         public string statusIdade { get; set; } = string.Empty;
         public string sexo { get; set; } = string.Empty;
-        public int tempoSecs { get; set; }
         public int descontoSalario { get; set; }
         public int pcd { get; set; }
+        public int tempoSecs { get; set; } 
 
     }
 }
